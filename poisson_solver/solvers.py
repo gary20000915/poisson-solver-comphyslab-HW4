fd2cb5cd9f0cdeccd951b1338959186ea27d2a68
Gary <garyphys0915@gapp.nthu.edu.tw>
2022-12-27 01:13:09 +0800

    finish the all code part

--- poisson_solver/solvers.py
+++ poisson_solver/solvers.py
@@ -10,17 +10,21 @@ Solver to solve for Laplace/Poisson's equation
 """
 
 @njit(parallel=True)
-def generate_g(g, N, x, y):
-    for i in prange(N):
-        for j in prange(N):
+def generate_g(g, buff_size, nx, ny, x, y):
+    for i in prange(nx+2*buff_size):
+        for j in prange(ny+2*buff_size):
             r1 = np.square(x[i] + 1.5) + np.square(y[j])
             r2 = np.square(x[i] - 1.5) + np.square(y[j])
             g[i, j] = np.exp(-5 * 0.25 * np.square(r1)) + 3 * 0.5 * np.exp(-np.square(r2))
 
     return g
 
-def set_boundary(nx, ny, buff_size, boundary, mesh: Mesh2D):
-    mesh[0, :]  = boundary[0]
+def set_boundary(g, x, y, nx, ny, buff_size, mesh: Mesh2D):
+    generate_g(g, buff_size, nx, ny, x, y)
+    UL = g[-1,:]
+    LR = g[:,-1]
+    boundary = np.array([[UL],[LR],[UL],[LR]])
+    mesh[0, :] = boundary[0]
     mesh[ny+buff_size, :] = boundary[1]
     mesh[:, 0]  = boundary[2]
     mesh[:, nx+buff_size] = boundary[3]
@@ -46,16 +50,25 @@ def gs_kernel(u, x, y, nx, ny, g, buff_size):
             u[i, j] = 0.25 * (u[i+1, j] + u[i, j+1] + u[i-1, j] + u[i, j-1] + g[i, j])
     return u
 
-def solve(name, tor, boundary, mesh: Mesh2D):
+@njit(parallel = True)
+def SOR_kernel(u, u_temp, x, y, nx, ny, g, buff_size, w):
+    for i in prange(1, int(nx + 2*buff_size - 1), 1):
+        for j in prange(1, int(ny + 2*buff_size - 1), 1):
+            r1 = np.square(x[i] + 1.5) + np.square(y[j])
+            r2 = np.square(x[i] - 1.5) + np.square(y[j])
+            g[i, j] = np.exp(-5 * 0.25 * np.square(r1)) + 3 * 0.5 * np.exp(-np.square(r2))
+            u[i, j] = 0.25 * (u[i+1, j] + u[i, j+1] + u[i-1, j] + u[i, j-1] + g[i, j])
+            u[i, j] = (1-w) * u_temp[i,j] + w * u[i, j]
+    return u
+
+def solve(name, tor, mesh: Mesh2D, **kwargs):
     u         = mesh.get_mesh()
     x         = mesh.get_x()
-    y         = mesh.get_y()   
+    y         = mesh.get_y()
     nx        = mesh.get_nx()
     ny        = mesh.get_ny()
     g         = mesh.get_xx()
     buff_size = mesh.get_buff_size()
-    
-    set_boundary(nx, ny, buff_size, boundary, u)
     
     err     = 10
     err_arr = np.array([])
@@ -63,11 +76,14 @@ def solve(name, tor, boundary, mesh: Mesh2D):
     
     while err > tor:
         u_temp = np.copy(u)
+        set_boundary(g, x, y, nx, ny, buff_size, u)
         
         if name == "Jacobi":
             u = j_kernel(u, u_temp, x, y, nx, ny, g, buff_size)
         elif name == "Gauss":
             u = gs_kernel(u, x, y, nx, ny, g, buff_size)
+        elif name == "SOR":
+            u = SOR_kernel(u, u_temp, x, y, nx, ny, g, buff_size, kwargs['w'])
         else:
             print("Error: unknown kernel!")
             break
@@ -94,10 +110,10 @@ if __name__=='__main__':
     nx, ny = 4, 4
     buff_size=1
     tor = 1e-10
-    boundary = np.zeros((4, nx + 2*buff_size))
+    # boundary = np.zeros((4, nx + 2*buff_size))
     # boundary[0] =np.ones(nx + 2*buff_size)
     mesh = Mesh2D(nx = nx, ny = ny, buff_size=buff_size)
 
-    u = solve("Gauss", tor, boundary, mesh)[1]
+    u = solve("Jacobi", tor, mesh)[1]
     print(u)
     print("TEST")
